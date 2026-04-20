a3cf163bfafae542603440c1ddd67fbf2fbd3e61
Tianhao Wu <tianhaowu@Mac.lan>
2026-04-20 16:10:33 -0700

    add bool aggregator

--- run_monitoring.py
+++ run_monitoring.py
@@ -107,7 +107,8 @@ algebras = {"minmax": MinMaxAlgebra(),
 
 aggregators = {"minmax": "min_max",
                 "count": "counting",
-                "avg": "averaging"
+                "avg": "averaging",
+                "bool": "boolean"
 }
 
 
